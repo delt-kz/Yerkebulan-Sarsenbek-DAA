2f4bfe04d435545f15f4c5d802ce568197bd75dc
delt-kz <yerkebulansarsenbek@yandex.kz>
2025-09-18 17:25:33 +0500

    Add ClosestPair
    Add Point2D

---
+++ src/main/java/ru/yandex/practicum/client/Point2D.java
@@ -0,0 +1,20 @@
+package ru.yandex.practicum.client;
+
+public class Point2D {
+    private final double x;
+    private final double y;
+
+    public Point2D(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() { return x; }
+    public double getY() { return y; }
+
+    public double distanceTo(Point2D other) {
+        double dx = x - other.x;
+        double dy = y - other.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+}
